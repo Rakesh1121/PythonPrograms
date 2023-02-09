b6424d67ce222bf768c3a736f0183818669ef9a6
Rakesh1121 <rakeshvish6636@gmail.com>
2023-02-09 16:29:18 +0530

    Commit

---
+++ Conditional/Greater3Num.py
@@ -0,0 +1,10 @@
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+num3 = input("Enter the third number: ")
+
+if num1>num2 and num1>num3:
+    print("First number is Greater number: ", num1)    
+elif num2>num1 and num2>num3:
+    print("Second number is Greater number: ",num2)    
+else:
+    print("Third number is Greater number: ",num3)    
---
+++ Dictionaries&Sets/Sets.py
@@ -0,0 +1,21 @@
+#Set is the collection of the non-repeatative element.
+
+s = set()
+s.add(1)
+s.add(2)
+s.add(2)#this item is not add in the set.
+print(s)
+
+name = {4, 7, 1, 67, 98, 'vinit'}
+print(name)
+
+print(len(name))
+
+'''
+operation in sets
+
+len()
+name.remove()
+name.pop
+name.clear() = empty the set    
+'''
