2cfeba258313a8bd24ceb0f6ebc23eb7ba3e67a2
Rakesh1121 <rakeshvish6636@gmail.com>
2023-02-12 19:05:23 +0530

    Commit

---
+++ Functions/first.py
@@ -0,0 +1,13 @@
+def Input():
+    pass
+    
+def sum(num1, num2):
+    sum = num1 + num2
+    return sum
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+a = sum(num1, num2)
+print(a)
+
---
+++ Loops/forloop.py
@@ -0,0 +1,11 @@
+li = [3,5,6,7]
+
+for i in li:
+    print(i)
+    
+
+
+li2 = ["Rakesh", "Vinit", "Sonu"]    
+for i in li2:
+    print(i)
+    #print(li2)
